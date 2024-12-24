5ce9d63c1f6ec3ba4a9623d3ca3a712ea01a9759
kaarthik-sns <kaarthikr@searchnscore.com>
2024-12-24 10:36:58 +0530

    dec 24

--- src/components/UserTable/UserTable.tsx
+++ src/components/UserTable/UserTable.tsx
@@ -143,44 +143,10 @@ const UserTable = () => {
                 </table>
             </div>
             <div className="max-w-screen-xl mx-auto mt-12 px-4 text-gray-600 md:px-8">
-                <div className="hidden justify-between text-sm md:flex">
-                    <div>
-                        SHOWING 1-10 OF 120
-                    </div>
-                    <div className="flex items-center gap-12" aria-label="Pagination">
-                        <a href="javascript:void(0)" className="hover:text-indigo-600">
-                            Previous
-                        </a>
-                        <ul className="flex items-center gap-1">
-                            {
-                                pages.map((item, idx) => (
-                                    <li key={item}>
-                                        {
-                                            item == "..." ? (
-                                                <div>
-                                                    {item}
-                                                </div>
-                                            ) : (
-
-                                                <a href="javascript:void(0)" aria-current={currentPage == item ? "page" : false} className={`px-3 py-2 rounded-lg duration-150 hover:text-white hover:bg-indigo-600 ${currentPage == item ? "bg-indigo-600 text-white font-medium" : ""}`}>
-                                                    {item}
-                                                </a>
-                                            )
-                                        }
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                        <a href="javascript:void(0)" className="hover:text-indigo-600">
-                            Next
-                        </a>
-                    </div>
-                </div>
-                {/* On mobile version */}
-                <div className="flex items-center justify-between text-sm text-gray-600 font-medium md:hidden">
+                <div className="flex items-center justify-between text-sm text-gray-600 font-medium">
                     <a href="javascript:void(0)" className="px-4 py-2 border rounded-lg duration-150 hover:bg-gray-50">Previous</a>
-                    <div className="font-medium">
-                        SHOWING 1-10 OF 120
+                    <div>
+                        Page {currentPage} of {pages.length}
                     </div>
                     <a href="javascript:void(0)" className="px-4 py-2 border rounded-lg duration-150 hover:bg-gray-50">Next</a>
                 </div>
