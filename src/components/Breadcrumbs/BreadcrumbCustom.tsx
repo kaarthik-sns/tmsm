ddc4491d6b67d968f3af1264bd15c91cbc25b184
BoobathiSNS <129281710+BoobathiSNS@users.noreply.github.com>
2025-02-18 18:27:34 +0530

    CMS Heading color changes done

--- src/components/Breadcrumbs/BreadcrumbCustom.tsx
+++ src/components/Breadcrumbs/BreadcrumbCustom.tsx
@@ -12,7 +12,7 @@ interface BreadcrumbProps {
 const Breadcrumb = ({ breadcrumbs }: BreadcrumbProps) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+      <h2 className="text-title-md2 font-semibold text-black dark:text-white dark-text">
         {breadcrumbs[breadcrumbs.length - 1]?.name || ""}
       </h2>
 
