6639cd2ab4547e05682166ec6bf82790c9d0726b
kaarthik-sns <kaarthikr@searchnscore.com>
2026-03-16 16:38:51 +0530

    update package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 import "jsvectormap/dist/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
 import "@/css/noto.css";
 import "@/css/style.css";
 import "quill/dist/quill.snow.css";
